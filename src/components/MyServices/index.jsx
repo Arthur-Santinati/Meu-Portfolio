30e5829d721d4dc5b85ec9ce261171c56e1f5907
arthur santinati <arthursantinati02@outlook.com>
2023-01-17 20:40:24 -0300

    concertando detalhes

--- src/components/MyServices/index.jsx
+++ src/components/MyServices/index.jsx
@@ -28,19 +28,16 @@ export const MyServices = () => {
     return (
         <section id='services' className='services'>
             <div className='container'>
-
                 <div className='section-title d-flex justify-content-center' data-aos='fade-up' data-aos-duration="1700">
                     <h2>Meus Projetos</h2>
-                    {/* <p>Nós da Casttrof oferecemos serviços de ponta, que vão desde cursos a CYBER SEGURANÇA e LGPD.</p> */}
                 </div>
-
                 <div className='row'>
                     <div className='col-lg-12 col-md-12 card-project' data-aos='fade-down-right' data-aos-duration="1700" onClick={() => handleClickTrybeTunes()}>
                         <div className='icon-box' id="trybe-tunes">
                             <div className='icon'><i class="bi bi-music-note-beamed"></i></div>
                             <h4 className='title'>TrybeTunes</h4>
                             <p className='description'>
-                                Neste projeto feito no curso da trybe individualmente fizemos uma brincadeira como se fosse um aplicativo de música parecido com o Spotify, com funcionalidades de poder buscar músicas, favorita-las, e procurar pelo álbum, ele não está com o estilo 100% terminado porém está funcional
+                                Neste projeto feito no curso da trybe individualmente fizemos uma brincadeira como se fosse um aplicativo de música parecido com o Spotify, com funcionalidades de poder buscar músicas, favorita-las, e procurar pelo álbum, ele não está com o estilo 100% terminado porém está funcional. (Dísponivel apenas para PC)
                             </p>
                         </div>
                     </div>
@@ -50,7 +47,7 @@ export const MyServices = () => {
                             <div className='icon'><i className='bi bi-card-checklist'></i></div>
                             <h4 className='title'>Tecer</h4>
                             <p className='description'>
-                                Neste projeto feito por mim e alguns outros colaboradores da Dopster,no meu emprego atual como desenvolvedor front end, o cliente pediu uma "ATS", que seria um Applicant tracking system, que traduzindo é um sistema de rastreamento de candidatos que permite o tratamento eletrônico das necessidades de recrutamento e contratação. Com funcionalidades de login, registro, casos para esquecer a senha, perfil, adicionar empresas como clientes e várias outras, projeto ainda em andamento.
+                                Neste projeto feito por mim e alguns outros colaboradores da Dopster,no meu emprego atual como desenvolvedor front end, o cliente pediu uma "ATS", que seria um Applicant tracking system, que traduzindo é um sistema de rastreamento de candidatos que permite o tratamento eletrônico das necessidades de recrutamento e contratação. Com funcionalidades de login, registro, casos para esquecer a senha, perfil, adicionar empresas como clientes e várias outras, projeto ainda em andamento. (Dísponivel apenas para PC)
                             </p>
                         </div>
                     </div>
@@ -60,7 +57,7 @@ export const MyServices = () => {
                             <div className='icon'><i class="bi bi-currency-exchange"></i></div>
                             <h4 className='title'>Thinksus</h4>
                             <p className='description'>
-                                Neste projeto feito também durante meu emprego como desenvolvedor, junto há um time de desenvolvedores, o cliente trabalha com indicadores esg que são as 3 siglas para: Governança ambiental, social e corporativa que são uma abordagem para avaliar até que ponto uma corporação trabalha em prol de objetivos sociais que vão além do papel de uma corporação para maximizar os lucros em nome dos acionistas da corporação. Por onde são feitos várias perguntas para avaliar se a empresa é sustentavel ou não, com funcionalidades de login, registro, tradução de linguagem, perfil e várias outras, feito com JavaScript, React, SCSS e HTML.
+                                Neste projeto feito também durante meu emprego como desenvolvedor, junto há um time de desenvolvedores, o cliente trabalha com indicadores esg que são as 3 siglas para: Governança ambiental, social e corporativa que são uma abordagem para avaliar até que ponto uma corporação trabalha em prol de objetivos sociais que vão além do papel de uma corporação para maximizar os lucros em nome dos acionistas da corporação. Por onde são feitos várias perguntas para avaliar se a empresa é sustentavel ou não, com funcionalidades de login, registro, tradução de linguagem, perfil e várias outras, feito com JavaScript, React, SCSS e HTML.(Dísponivel apenas para PC)
                             </p>
                         </div>
                     </div>
@@ -69,7 +66,7 @@ export const MyServices = () => {
                             <div className='icon'><i class="bi bi-egg-fried"></i></div>
                             <h4 className='title'>Encookado</h4>
                             <p className='description'>
-                                Desenvolvemos um site complexo de busca de receitas de diversas comidas, podendo visualizar, buscar, filtrar, favoritar e acompanhar o progresso de preparação das receitas e drinks, utilizados ferramentas de dentro do ecossistema React: Hooks e Context API, utilizamos 2 APi's, uma para comidas e outra para bebidas, utilizamos também redux, e gerenciamento de estados, e também a estilização geral feita com CSS.
+                                Desenvolvemos um site complexo de busca de receitas de diversas comidas, podendo visualizar, buscar, filtrar, favoritar e acompanhar o progresso de preparação das receitas e drinks, utilizados ferramentas de dentro do ecossistema React: Hooks e Context API, utilizamos 2 APi's, uma para comidas e outra para bebidas, utilizamos também redux, e gerenciamento de estados, e também a estilização geral feita com CSS. (Dísponivel apenas para PC)
                             </p>
                         </div>
                     </div>
@@ -78,8 +75,7 @@ export const MyServices = () => {
                             <div className='icon'><i class="bi bi-magic"></i></div>
                             <h4 className='title'>TrybeWarts</h4>
                             <p className='description'>
-
-                                Neste projeto desenvolemos eu e um amigo do curso, uma página de preenchimento de formulário da Escola de magia de Trybewarts, em que as pessoas estudantes poderão enviar seus feedbacks sobre ela. O tema desse projeto é baseado na obra 'Harry Potter', de J. K. Rowling, já que programar é o mais próximo que podemos chegar de algo verdadeiramente mágico! Feito utilizando apenas HTML, JavaScript e CSS
+                                Neste projeto desenvolemos eu e um amigo do curso, uma página de preenchimento de formulário da Escola de magia de Trybewarts, em que as pessoas estudantes poderão enviar seus feedbacks sobre ela. O tema desse projeto é baseado na obra 'Harry Potter', de J. K. Rowling, já que programar é o mais próximo que podemos chegar de algo verdadeiramente mágico! Feito utilizando apenas HTML, JavaScript e CSS. (Dísponivel apenas para PC)
                             </p>
                         </div>
                     </div>
